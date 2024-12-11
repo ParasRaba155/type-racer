d0684f98d3ff8af65644383cfa233eb48a32df3b
ParasRaba <parasgadhvi12345@gmail.com>
2024-12-11 22:49:55 +0530

    feat: identify and tryout the `term` package

--- main.go
+++ main.go
@@ -1,4 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/term"
+)
+
 func main() {
+	oldState, err := term.MakeRaw(int(os.Stdout.Fd()))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		err := term.Restore(int(os.Stdin.Fd()), oldState)
+		fmt.Printf("\n error in restoring: %v", err)
+	}()
+
+	terminal := term.NewTerminal(os.Stdout, "Ready for the race??")
+	terminal.SetPrompt(GetRandomText())
+	terminal.ReadLine()
 }
