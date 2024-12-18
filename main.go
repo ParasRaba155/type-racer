1cec306c13e262f638751ee2f3cb7064a3d5e4e0
ParasRaba <parasgadhvi12345@gmail.com>
2024-12-18 22:26:03 +0530

    feat: handle input of chars with colors
    - feat: add helpful terminal ascii chars

--- main.go
+++ main.go
@@ -8,7 +8,24 @@ import (
 	"golang.org/x/term"
 )
 
-const greetingMessage = "Ready For the Race!!!!!"
+const (
+	greetingMessage = "Ready For the Race!!!!!"
+
+	// ascii escape chars for colors
+	grayColor  = "\033[37m"
+	resetColor = "\033[0m"
+	redColor   = "\033[31m"
+	greenColor = "\033[32m"
+
+	// carriage chars
+	carriageReturn  = "\r"
+	carriageNewLine = "\r\n"
+
+	// special chars
+	hideCursor        = "\033[?25l"
+	showCursor        = "\033[?25h"
+	deleteTillNewLine = "\033[K"
+)
 
 func main() {
 	fd := os.Stdin.Fd()
@@ -23,34 +40,69 @@ func main() {
 	}
 	defer func() {
 		err := term.Restore(int(fd), oldState)
-		fmt.Printf("\n error in restoring: %v", err)
+		fmt.Printf(showCursor)
+		log.Printf("\n error in restoring: %v", err)
 	}()
 
 	text := GetRandomText()
+	text = abcd()
 
-	fmt.Println(greetingMessage)
-	fmt.Println(text)
+	fmt.Print(grayColor)
+	fmt.Print(greetingMessage, carriageNewLine)
+	fmt.Print(resetColor)
+	fmt.Print(grayColor)
+	fmt.Print(text)
+	fmt.Print(resetColor)
+	fmt.Print(carriageReturn)
+	fmt.Print(hideCursor)
+
 	userInput := make([]rune, len(text))
 	pos := 0
 
 	for pos < len(text) {
 		buf := make([]byte, 1)
-		os.Stdin.Read(buf)
+		n, err := os.Stdin.Read(buf)
+		if n != 1 || err != nil {
+			log.Printf("reading buffer: %d: %v", n, err)
+			return
+		}
 
 		char := buf[0]
 
-		if char == 127 {
-			fmt.Print("\b \b")
-			continue
-		}
-
-		if char != text[pos] {
-			fmt.Printf("diff: %s, %s\n", string(char), string(text[pos]))
+		if char != 127 {
+			userInput[pos] = rune(char)
 			pos++
-			continue
+		} else {
+			pos = max(pos-1, 0)
 		}
-		pos++
-		userInput[pos] = rune(char)
 
+		fmt.Print(carriageReturn)
+		for i, char := range text {
+			if i > len(userInput) {
+				fmt.Print(grayColor)
+				fmt.Printf("%c", char)
+				fmt.Print(resetColor)
+				continue
+			}
+			if userInput[i] == char {
+				fmt.Print(greenColor)
+				fmt.Printf("%c", char)
+				fmt.Print(resetColor)
+				continue
+			}
+			fmt.Print(redColor)
+			fmt.Printf("%c", char)
+			fmt.Print(resetColor)
+		}
 	}
+	fmt.Print(resetColor)
+}
+
+func abcd() string {
+	var c [26]byte
+	var i byte
+	for i = 65; i <= 90; i++ {
+		c[i-65] = i
+	}
+	return string(c[:])
 }
